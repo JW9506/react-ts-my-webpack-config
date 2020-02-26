79089d21613c7cf5b7ab190623d448e7e9398df0
JW9506 <cf.dota@gmail.com>
2020-02-26 11:45:02 -0500

    base

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import "../css/main.less";
 
-function App(props: { name: string, age: number }) {
-  console.log(props.name);
+function App() {
   return (
     <>
       <div>Hello</div>
-      <img
-        src={require("../imgs/1200px-Avenue_of_Stars_Hong_Kong_Bruce_Lee_Statue.jpg")}
-        alt=""
-      />
-      <div id="pic"></div>
     </>
   );
 }
